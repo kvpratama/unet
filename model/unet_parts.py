5fccf627aa5b6fc310abb63abe13a457bfde6538
Kevin Pratama <kvpratama@gmail.com>
2020-05-15 17:31:24 +0900

    make recurrent block class

--- model/unet_parts.py
+++ model/unet_parts.py
@@ -126,11 +126,9 @@ class AttentionBlock(nn.Module):
         return x * resampler
 
 
-class RecurrentConv(nn.Module):
-    def __init__(self, in_channels, out_channels, t=2):
-        super(RecurrentConv, self).__init__()
-        self.t = t
-        self.inconv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+class RecurrentBlock(nn.Module):
+    def __init__(self, out_channels):
+        super(RecurrentBlock, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(out_channels),
@@ -138,8 +136,19 @@ class RecurrentConv(nn.Module):
         )
 
     def forward(self, x):
+        return self.conv(x)
+
+
+class RecurrentConv(nn.Module):
+    def __init__(self, in_channels, out_channels, t=2):
+        super(RecurrentConv, self).__init__()
+        self.t = t
+        self.inconv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0)
+        self.rec_block = RecurrentBlock(out_channels)
+
+    def forward(self, x):
         x_in = self.inconv(x)
-        x_out = self.conv(x_in)
+        x_out = self.rec_block(x_in)
         for i in range(self.t):
-            x_out = self.conv(x_in + x_out)
+            x_out = self.rec_block(x_in + x_out)
         return x_out
